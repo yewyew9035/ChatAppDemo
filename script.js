1b8f04ecd258754fc623b3b3ebe930e18ec3e2dc
Hugo-Ng <yew_yew@live.com>
2019-11-20 15:52:38 +0800

    aa

--- script.js
+++ script.js
@@ -2,29 +2,55 @@ const socket = io('http://localhost:3000')
 const messageContainer = document.getElementById('message-container')
 const messageForm = document.getElementById('send-container')
 const messageInput = document.getElementById('message-input')
-
+const feedback = document.getElementById('feedback')
+const onlineUser = document.getElementById('online-user');
 const name = prompt('What is your name?')
 appendMessage('You Joined')
 socket.emit('new-user', name)
 socket.on('chat-message', data => {
     appendMessage(data.name + ":" + data.message)
+    feedback.innerHTML = '';
 })
-socket.on('user-connected', name => {
-    appendMessage(name + '  connected')
+
+// listener when user connected to the server
+socket.on('user-connected', data => {
+    appendMessage(data.name + '  connected')
+    // console.log(data.users);
+    // var allOnlineUser = '';
+    // for(user in data.users){
+    //     allOnlineUser=allOnlineUser+user.value;
+    // }
+
+    // onlineUser.innerHTML = allOnlineUser;
 })
+// listener when user disconnected from the server
 socket.on('user-disconnected', name => {
     appendMessage(name + '  disconnected')
 })
+
+// listener to display when other user is typing
+socket.on('user-is-typing', name => {
+    feedback.innerHTML = '</br><p><em><b>' + name + '</b> is typing a message...</em></p>'
+})
+// submit event
 messageForm.addEventListener('submit', e => {
+
     e.preventDefault();
     const message = messageInput.value
-    appendMessage('You : '+message)
+    appendMessage('You : ' + message)
     socket.emit('send-chat-message', message)
     messageInput.value = ''
+
 })
 
+// append any message to message container div
 function appendMessage(message) {
-    const messageElement = documsssent.createElement('div')
+    const messageElement = document.createElement('div')
     messageElement.innerText = message
     messageContainer.append(messageElement)
 }
+
+// to catch keypress function
+messageInput.addEventListener('keypress', function () {
+    socket.emit('typing-message', name)
+})
--- server.js
+++ server.js
@@ -1,16 +1,22 @@
 const io = require('socket.io')(3000)
+
+// user who is online now
 const users = {}
+
 io.on('connection', socket => {
     socket.on('new-user', name => {
         users[socket.id] = name
-        socket.broadcast.emit('user-connected', name)
+        socket.broadcast.emit('user-connected', { users: users, name: name })
     })
     socket.on('send-chat-message', message => {
         socket.broadcast.emit('chat-message',
-         { message: message, name: users[socket.id] });
+            { message: message, name: users[socket.id] });
     })
-    socket.on('disconnect',()=>{
-        socket.broadcast.emit('user-disconnected',users[socket.id])
+    socket.on('disconnect', () => {
+        socket.broadcast.emit('user-disconnected', users[socket.id])
         delete users[sockets.id]
+    })
+    socket.on('typing-message', function (data) {
+        socket.broadcast.emit('user-is-typing', data)
     })
 })  
